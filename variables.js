6ba73f407fbd8b784b3e88e4e5b8814a09af4426
Prathik Shetty <prathikkshetty15@gmail.com>
2022-11-23 06:17:27 +0000

    changes

---
+++ variables.js
@@ -0,0 +1,28 @@
+//Variables in JS
+
+// var p = "prathi'k"; //string
+// const ps = "prathik"; //string but the value is constant
+// NOtPrathik = "not prathik"; //undefined
+// console.log(typeof NotPrathik); //printing to console
+
+// let number = 17; //let is a block case
+// console.log(typeof number, number);
+
+// console.log(typeof p, typeof ps, p);
+// let bol = true; //boolean
+// let bol1 = 0; //number
+// console.log(typeof bol, typeof bol1);
+// let me = null; //null
+
+//Conditional statements
+
+// const val = 5;
+// if (val > 10) {
+//   console.log("greater than 10");
+// } else {
+//   console.log("less than 10");
+// }
+
+const fruits = [];
+fruits.push("apple", "banana", "grapes");
+console.log(fruits);
